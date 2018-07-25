8f6d2aa4da71a16ddaf0ba7eb1f92e117aedd348
Kerr Ivan Agapito Cirilo <kerr.ivan29@gmail.com>
2018-07-25 07:12:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo (name) {
   return `Happy Holidays, ${name}!`
 }
 
-function happyHolidayTo (name) {
+function happyHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
